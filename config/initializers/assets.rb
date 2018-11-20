34e58044ca8188c944f42640209e1343b9b5ae28
Jalen Ong <jalen.ong@gmail.com>
2018-11-20 15:54:30 +1100

    Asset precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( results.scss )
+Rails.application.config.assets.precompile += %w( 'application2.css', 'scaffolds.scss', 'toolkit-light.css', 'application2.js', 'chart.js', 'cable.js', 'jquery.min.js', 'popper.min.js' )
