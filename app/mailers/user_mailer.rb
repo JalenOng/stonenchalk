00352cbc2c11014fe61bde96e0409a9479077600
Jalen Ong <jalen.ong@gmail.com>
2018-12-06 18:21:13 +1100

    Change button color

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,12 +5,22 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.reporting.subject
   #
-  def reporting(pdf=nil)
+  def reporting(results, project_outcomes, kpis, recipient_commitments, milestones_1, milestones_2, milestones_3, jobs, events, amount, occupancy)
     @greeting = "Hi"
+    @results = results
+    @project_outcomes = project_outcomes
+    @kpis = kpis
+    @recipient_commitments = recipient_commitments
+    @milestones_1 = milestones_1
+    @milestones_2 = milestones_2
+    @milestones_3 = milestones_3 
+    @jobs = jobs
+    @events = events 
+    @amount = amount
+    @occupancy = occupancy
     # attachments["KPI_Stone&Chalk.pdf"] = WickedPdf.new.pdf_from_string(
     #   render_to_string(pdf: 'KPI' + Time.now.strftime('%v %H:%M:%S').to_s, template: "results/pdf.html.erb")
-    attachments['filename.pdf'] = pdf if pdf.present?
-
+ 
     mail to: ENV["MAILTO"], subject: "STONE AND CHALK KPI UPDATE"
   end
 end
