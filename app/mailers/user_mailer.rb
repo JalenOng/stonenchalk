2170deb7943a16728a15bd6e4f39811d9db35e59
Jalen Ong <jalen.ong@gmail.com>
2018-12-10 15:45:05 +1100

    Minor

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,20 +9,6 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.reporting.subject
   #
   def reporting
-    from = Email.new(email: 'grantreporting@stoneandchalk.com.au')
-    to = Email.new(email: ENV['MAILTO'])
-    subject = 'Stone and Chalk GSN Government Reporting'
-    # content = Content.new(type: 'text/plain', value: 'This is the latest update from Stone & Chalk GSN Victorian Government Reporting')
-    content = Content.new(
-      type: 'text/html',
-      value: ApplicationController.render(
-        template: "user_mailer/reporting.html.erb"
-      ))
-    
-    mail = Mail.new(from, subject, to, content)
-
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
     # attachments["KPI_Stone&Chalk.pdf"] = WickedPdf.new.pdf_from_string(
     #   render_to_string(pdf: 'KPI' + Time.now.strftime('%v %H:%M:%S').to_s, template: "results/pdf.html.erb")
  
