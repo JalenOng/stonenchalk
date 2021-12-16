b0d97bb84d47684124526e37e157e2521ea298b4
Jalen Ong <jalen.ong@gmail.com>
2021-12-16 10:58:17 +0800

    Replaced rails 5.2 files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get 'static_pages/Home'
+  get 'static_pages/graph'
+
+  resources :results
+  
+  root 'results#index'
+
+  resources :companies, only: ["index"]
+
 end
